7aa4471027df34fd4212a36a25f0df6b3b8d2f0c
xinxilope <mateuseidt10@gmail.com>
2022-09-22 18:12:19 -0300

    Update datas.py

--- exercicios cursos alura/06-validacao padrao nacional/datas.py
+++ exercicios cursos alura/06-validacao padrao nacional/datas.py
@@ -5,27 +5,6 @@ class DatasBr:
     def __init__(self):
         self.momento_cadastro = datetime.today()
 
-    # def mes_cadastro(self):
-    #     meses_do_ano = [
-    #         "janeiro", "fevereiro", "março",
-    #         "abril", "maio", "junho",
-    #         "julho", "agosto", "setembro",
-    #         "outubro", "novembro", "dezembro"
-    #     ]
-
-    #     mes_cadastro = self.momento_cadastro.month - 1
-    #     return meses_do_ano[mes_cadastro]
-
-    # def dia_semana(self):
-    #     dia_semana_lista = [
-    #         "segunda", "terça",
-    #         "quarta", "quinta", "sexta",
-    #         "sábado", "domingo"
-    #     ]
-
-    #     dia_semana = self.momento_cadastro.weekday()
-    #     return dia_semana_lista[dia_semana]
-
 #METODOS PARA REPRESENTAR OBJETO
     def __str__(self):
         return self.format_data()
@@ -33,7 +12,3 @@ class DatasBr:
     def format_data(self):
         data_formatada = self.momento_cadastro.strftime("%d/%m/%Y %H:%M")
         return data_formatada
-
-    # def tempo_cadastro(self):
-    #     tempo_cadastro = datetime.today() - self.momento_cadastro
-    #     return tempo_cadastro
