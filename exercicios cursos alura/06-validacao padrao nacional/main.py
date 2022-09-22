1f00389ee607a44b3c6782c4d994d8b48ee06417
xinxilope <mateuseidt10@gmail.com>
2022-09-22 16:13:11 -0300

    criando classe telefone

--- exercicios cursos alura/06-validacao padrao nacional/main.py
+++ exercicios cursos alura/06-validacao padrao nacional/main.py
@@ -1,3 +1,4 @@
+from telefones import Telefone
 from documento import Documento
 
 
@@ -8,4 +9,8 @@ pessoa2 = Documento.cria_documento('81188463000120')
 print(pessoa1)
 print(pessoa2)
 
+telefone1 = Telefone("1214912341234")
+telefone2 = Telefone("4312341234")
 
+print(telefone1)
+print(telefone2)
---
+++ exercicios cursos alura/06-validacao padrao nacional/telefones.py
@@ -0,0 +1,30 @@
+import re
+
+class Telefone:
+    def __init__(self, telefone):
+        if self.valida_telefone(telefone):
+            self.numero = telefone
+        else:
+            raise ValueError("Telefone invalido")
+
+#METODO USADO NO __INIT__
+    def valida_telefone(self, telefone):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.findall(padrao, telefone)
+        if resposta:
+            return True
+        else:
+            return False
+
+#METODOS PARA PRINTRAR OBJETO
+    def __str__(self):
+        return self.format()
+
+    def format(self):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.numero)
+        if resposta.group(1) == None:
+            numero_formatado = "+55 ({}) {}-{}".format(resposta.group(2),resposta.group(3),resposta.group(4))
+        else:
+            numero_formatado = "+{} ({}) {}-{}".format(resposta.group(1),resposta.group(2),resposta.group(3),resposta.group(4))
+        return numero_formatado
