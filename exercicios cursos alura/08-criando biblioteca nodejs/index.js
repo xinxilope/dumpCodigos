c77362b41ceb247204d9bd2811b008a9f93cd64e
xinxilope <mateuseidt10@gmail.com>
2022-09-29 17:16:22 -0300

    criando condicao de erro

--- exercicios cursos alura/08-criando biblioteca nodejs/index.js
+++ exercicios cursos alura/08-criando biblioteca nodejs/index.js
@@ -1,9 +1,16 @@
 import fs from 'fs'
 import chalk from 'chalk'
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code,"Arquivo nao encontrado"))
+}
+
 function pegaArquivo(caminhoDoArquivo) {
     const encode = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encode, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encode, (erro, texto) => {
+        if (erro) {
+            trataErro(erro)
+        }
         console.log(chalk.green(texto))
     })
 }
