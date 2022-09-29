1d9f9c120c68e7d1c44fad035fb19b0c56707c64
xinxilope <mateuseidt10@gmail.com>
2022-09-29 17:46:21 -0300

    Async function refactor

--- exercicios cursos alura/08-criando biblioteca nodejs/index.js
+++ exercicios cursos alura/08-criando biblioteca nodejs/index.js
@@ -7,12 +7,19 @@ function trataErro(erro) {
 
 function pegaArquivo(caminhoDoArquivo) {
     const encode = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encode, (erro, texto) => {
-        if (erro) {
-            trataErro(erro)
-        }
-        console.log(chalk.green(texto))
-    })
+    fs.promises.readFile(caminhoDoArquivo, encode)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encode = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encode, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro)
+//         }
+//         console.log(chalk.green(texto))
+//     })
+// }
 
 pegaArquivo('./arquivos/texto.md')
