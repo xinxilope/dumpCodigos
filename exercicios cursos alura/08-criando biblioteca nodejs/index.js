ceddbc79ce37dbebe2593004411fda7666062a05
xinxilope <mateuseidt10@gmail.com>
2022-09-30 18:13:10 -0300

    criando funcao para extrair links

--- exercicios cursos alura/08-criando biblioteca nodejs/index.js
+++ exercicios cursos alura/08-criando biblioteca nodejs/index.js
@@ -2,35 +2,25 @@ import fs from 'fs'
 import chalk from 'chalk'
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code,"Arquivo nao encontrado"))
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
     try {
-        const encode = 'utf-8'
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encode)
-        console.log(chalk.green(texto))
-    } catch (error) {
-        trataErro(error)
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(extraiLinks(texto));
+    } catch (erro) {
+        trataErro(erro)
     }
 }
-
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encode = 'utf-8'
-//     fs.promises.readFile(caminhoDoArquivo, encode)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encode = 'utf-8'
-//     fs.readFile(caminhoDoArquivo, encode, (erro, texto) => {
-//         if (erro) {
-//             trataErro(erro)
-//         }
-//         console.log(chalk.green(texto))
-//     })
-// }
 
 pegaArquivo('./arquivos/texto.md')
